18e41257dfc661ec376d9e757630b90b04fa83c4
yu <peacetomori1118@gmail.com>
2020-01-23 20:37:55 +0900

    add texts

--- src/components/RenderArticle.vue
+++ src/components/RenderArticle.vue
@@ -119,6 +119,22 @@ export default {
         line.geometry.computeBoundingSphere()
       }
       updatePoints()
+    },
+    drawTexts () {
+      const loader = new THREE.FontLoader()
+      loader.load('fonts/helvetiker_regular.typeface.json', function (font) {
+        const geometry = new THREE.TextGeometry('Hello three.js!', {
+          font: font,
+          size: 80,
+          height: 5,
+          curveSegments: 12,
+          bevelEnabled: true,
+          bevelThickness: 10,
+          bevelSize: 8,
+          bevelOffset: 0,
+          bevelSegments: 5
+        })
+      })
     }
   }
 }
